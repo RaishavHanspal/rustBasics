183ba163d0fe6696f9cdf3a3c8ecf1ba2225434b
Raishav Hanspal <rhanspal25@gmail.com>
2024-03-15 00:28:11 +0530

    add abd use rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+use std::io;
 fn main() {
     // using constants we cannot reassign the value, or redeclare
     const TIME: u32 = 1230;
@@ -15,13 +17,16 @@ fn main() {
     {
         let name = "Bowser";
         let age = age + 4;
-        println!(
-            "...Meanwhile, {} ({} years old), looking from a distance!",
-            name, age
-        )
+        println!("...Meanwhile, {name} ({age} years old), looking from a distance!")
     }
-    println!(
-        "Mucho Gusto {}, Yo Soy {}, I'm {} years old.",
-        mario, name, age
-    );
+    println!("Mucho Gusto {mario}, Yo Soy {name}, I'm {age} years old.");
+    let mut name = String::new();
+    let mut age = String::new();
+    println!("What's your name?");
+    io::stdin().read_line(&mut name).expect("name");
+    println!("Hi {} How old are you?", name);
+    // if we don't add use std::io at the top, we can directly access the same functionality prefixing std:: as used below
+    std::io::stdin().read_line(&mut age).expect("age");
+    let age = rand::thread_rng().gen_range(20..=50);
+    println!("Okay! I thought it would be {age}.");
 }

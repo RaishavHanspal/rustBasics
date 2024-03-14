d2016e2e32f6cf1ad570f53e509308482b15cd7f
Raishav Hanspal <rhanspal25@gmail.com>
2024-03-15 01:47:10 +0530

    add loop and use fn to segregate code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 fn main() {
+    prologue();
+}
+
+fn prologue() {
     // using constants we cannot reassign the value, or redeclare
     const TIME: u32 = 1230;
     // by default we cannot reassign the value, but we can redeclare same name variable
@@ -10,7 +14,7 @@ fn main() {
     let mut age = 26;
     // string literal '{}' is a must when printing variables/constants
     println!("Time: {}", TIME);
-    println!("Hola! Yo Soy {}, I'm {} years old.", name, age);
+    println!("[{}:] Hola! Yo Soy {}, I'm {} years old.", name, name, age);
     let mario = name;
     let name = "Luigi";
     age = 30;
@@ -20,20 +24,42 @@ fn main() {
         let age = age + 4;
         println!("...Meanwhile, {name} ({age} years old), looking from a distance!")
     }
-    println!("Mucho Gusto {mario}, Yo Soy {name}, I'm {age} years old.");
-    let mut name = String::new();
+    println!("[{name}:] Mucho Gusto {mario}, Yo Soy {name}, I'm {age} years old.");
+    your_info(mario);
+}
+
+fn your_info(host: &str) {
+    let mut name: String = String::new();
     let mut age = String::new();
-    println!("What's your name?");
+    println!("[{host}:] What's your name?");
     io::stdin().read_line(&mut name).expect("name");
-    println!("Hi {} How old are you?", name);
+    let name = name.trim();
+    println!("[{host}:] Hi {}, How old are you?", name);
     // if we don't add use std::io at the top, we can directly access the same functionality prefixing std:: as used below
     std::io::stdin().read_line(&mut age).expect("age");
     let age: u32 = age.trim().parse().expect("Please type a number!");
-    let guess_age = rand::thread_rng().gen_range(20..=50);
-    println!("Okay! I thought it would be {guess_age}.");
-    match guess_age.cmp(&age) {
-        Ordering::Equal => println!("See I was right!"),
-        Ordering::Greater => println!("You look Older than your Age!"),
-        Ordering::Less => println!("Wow, You have maintained well..."),
+    println!("...Your silence, makes {host} nervous");
+
+    let mut min_age = 0;
+    let mut max_age = 100;
+    let mut trials: u32 = 0;
+    loop {
+        let guess_age = rand::thread_rng().gen_range(min_age..=max_age);
+        println!("[{host}:] Is it {guess_age}?");
+        trials = trials + 1;
+        match guess_age.cmp(&age) {
+            Ordering::Equal => {
+                println!("[{name}:] Bingo...! It took you {trials} tries.");
+                break;
+            }
+            Ordering::Greater => {
+                println!("[{name}:] Do I look that old?");
+                max_age = guess_age - 1;
+            }
+            Ordering::Less => {
+                println!("[{name}:] Haha.. I'm older than you think!");
+                min_age = guess_age + 1;
+            }
+        }
     }
 }
